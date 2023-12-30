eab8ac6aa81437205c3f502ebf3aafa6c4d6c493
Jonathan Jenkins <jonathan.jenkins1@att.net]>
2023-12-29 22:58:03 -0500

    Committing my completed R file into my git repository and pushing my git branch to the GitHub repository under my account.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,107 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This second programming assignment will require you to write an R function is
+## able to cache potentially time-consuming computations. For example, taking the
+## mean of a numeric vector is typically a fast operation. However, for a very
+## long vector, it may take too long to compute the mean, especially if it has
+## to be computed repeatedly (e.g. in a loop). If the contents of a vector are
+## not changing, it may make sense to cache the value of the mean so that when
+## we need it again, it can be looked up in the cache rather than recomputed.
+## In this Programming Assignment will take advantage of the scoping rules of
+## the R language and how they can be manipulated to preserve state inside of
+## an R object.
+
+## Matrix inversion is usually a costly computation and there may be some
+## benefit to caching the inverse of a matrix rather than compute it repeatedly
+## (there are also alternatives to matrix inversion that we will not discuss
+## here). Your assignment is to write a pair of functions that cache the inverse
+## of a matrix.
+
+## Write the following functions:
+
+##      1. makeCacheMatrix: This function creates a special "matrix" object that
+##         can cache its inverse.
+##      2. cacheSolve: This function computes the inverse of the special
+##         "matrix" returned by makeCacheMatrix above. If the inverse has already
+##         been calculated (and the matrix has not changed), then the cachesolve
+##         should retrieve the inverse from the cache.
+
+## Computing the inverse of a square matrix can be done with the solve function
+## in R. For example, if X is a square invertible matrix, then solve(X) returns
+## its inverse.
+
+## For this assignment, assume that the matrix supplied is always invertible.
+
+## In order to complete this assignment, you must do the following:
+
+##      1. Fork the GitHub repository containing the stub R files at 
+##         https://github.com/rdpeng/ProgrammingAssignment2 to create a copy
+##         under your own account.
+
+##      2. Clone your forked GitHub repository to your computer so that you can
+##         edit the files locally on your own machine.
+
+##      3. Edit the R file contained in the git repository and place your
+##         solution in that file (please do not rename the file).
+
+##      4. Commit your completed R file into YOUR git repository and push your
+##         git branch to the GitHub repository under your account.
+
+##      5. Submit to Coursera the URL to your GitHub repository that contains
+##         the completed R code for the assignment.
+
+## In addition to submitting the URL for your GitHub repository, you will need
+## to submit the 40 character SHA-1 hash (as string of numbers from 0-9 and
+## letters from a-f) that identifies the repository commit that contains the
+## version of the files you want to submit. You can do this in GitHub by doing
+## the following
+
+##      1. Going to your GitHub repository web page for this assignment
+
+##      2. Click on the “?? commits” link where ?? is the number of commits you
+##         have in the repository. For example, if you made a total of 10
+##         commits to this repository, the link should say “10 commits”.
+
+##      3. You will see a list of commits that you have made to this repository.
+##         The most recent commit is at the very top. If this represents the
+##         version of the files you want to submit, then just click the
+##         “copy to clipboard” button on the right hand side that should appear
+##         when you hover over the SHA-1 hash. Paste this SHA-1 hash into the
+##         course web site when you submit your assignment. If you don't want
+##         to use the most recent commit, then go down and find the commit you
+##         want and copy the SHA-1 hash.
+
+## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) inv <<- inverse
+  getInverse <- function() inv
+  list(set = set,
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" I created in the 
+## above makeCacheMatrix. If the inverse has already been calculated (and the 
+## matrix has not changed), then it should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  if (!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  mat <- x$get()
+  inv <- solve(mat, ...)
+  x$setInverse(inv)
+  inv
 }
